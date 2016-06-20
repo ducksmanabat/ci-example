97cc07eaecf94b7fce9ccc0b35f256e9b56190e0
smac <sheldon>
2016-06-20 09:24:31 -0600

    added test

--- test/QuestionTests.js
+++ test/QuestionTests.js
@@ -35,4 +35,39 @@ suite('QuestionTests', function() {
       done();
     });
   });
+
+
+  // we will now write a new test and run it locally to see if it works
+  // tests passed :)
+  // we will now setup travis ci to automatically run our tests when we check into the repo
+  
+
+    test('#createQuestion', function(done) {
+      // assemble
+      var questionInputDto = new QuestionInputDto();
+      questionInputDto.questionId = 1;
+      questionInputDto.text = 'Do you like kittens?';
+      questionInputDto.choices = ['Yes','No'];
+
+      var questionEngine = new QuestionEngine();
+
+      var createQuestion = questionEngine.createQuestion(questionInputDto);
+      var findQuestion = questionEngine.findQuestionWithQuestionId(1);
+
+      // act
+      return createQuestion.then(findQuestion.then((question)=> {
+        // asserts
+        assert.isOk(question);
+        assert.equal(question.questionId, questionInputDto.questionId);
+        assert.equal(question.text, questionInputDto.text);
+        assert.equal(question.QuestionChoices[0].choiceId, 1);
+        assert.equal(question.QuestionChoices[0].text, 'Yes');
+        assert.equal(question.QuestionChoices[1].choiceId, 2);
+        assert.equal(question.QuestionChoices[1].text, 'No');
+        done();
+      }));
+    });
+
+
+
 });
